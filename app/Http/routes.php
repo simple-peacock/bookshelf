0fd76d108a0545dfb47ac67f835e53941429ceb6
Peter <peter@simplepeacock.com>
2015-09-24 13:34:28 +0000

    Added home route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', function () {
+    return redirect('/book'); // no home page - redirect to our index of borrowed books
+});
+
 // RESTful route for our book resource - only using 3 routes
 Route::resource('book', 'BookController',['only' => ['index', 'create', 'store']]);
 
