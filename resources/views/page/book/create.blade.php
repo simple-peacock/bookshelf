c54f58a28971e62537de9c29498d20ae34a6fc91
Peter <peter@simplepeacock.com>
2015-09-24 13:35:32 +0000

    Final touches on the views

--- resources/views/page/book/create.blade.php
+++ resources/views/page/book/create.blade.php
@@ -3,5 +3,56 @@
 @section('title', 'Bookshelf - Borrow a book')
 
 @section('content')
-    <p>This is the page for borrowing a book</p>
+
+    <h3>Borrow Book</h3>
+
+    <form method="post" action="{{ action('BookController@store') }}">
+
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="title" class="right inline">Book Title: *</label>
+            </div>
+            <div class="small-9 columns">
+                <input type="text" name="title" placeholder="Title" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="author" class="right inline">Book Author: *</label>
+            </div>
+            <div class="small-9 columns">
+                <input type="text" name="author" placeholder="Author" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="barcode" class="right inline">Barcode/ISBN: *</label>
+            </div>
+            <div class="small-9 columns">
+                <input type="text" name="barcode" placeholder="Barcode" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="name" class="right inline">Your Name: *</label>
+            </div>
+            <div class="small-9 columns">
+                <input type="text" name="name" placeholder="Name" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-3 columns">
+                <label for="email" class="right inline">Your Email: *</label>
+            </div>
+            <div class="small-9 columns">
+                <input type="email" name="email" placeholder="Email" required>
+            </div>
+        </div>
+        <div class="row text-center">
+            <button type="submit" class="button radius success">OK</button>
+            <a href="{{ URL::previous() }}" class="button alert radius">Cancel</a>
+        </div>
+    </form>
 @endsection
--- resources/views/page/book/index.blade.php
+++ resources/views/page/book/index.blade.php
@@ -3,5 +3,36 @@
 @section('title', 'Bookshelf')
 
 @section('content')
-    <p>This is the index view</p>
+
+    <h3>Currently Borrowed Books</h3>
+
+    @if (count($books)!=0)
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Book Title</th>
+                    <th>Book Author</th>
+                    <th>Borrowed By</th>
+                    <th>Borrowed Date</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book)
+                    <tr>
+                        <td>{{ $book->title }}</td>
+                        <td>{{ $book->author }}</td>
+                        <td>{{ $book->name }}</td>
+                        <td>{{ date('d/m/Y', strtotime($book->borrowed_at)) }}</td>
+                        <td><a href="{{ action('BookController@returnBook',['id'=>$book->id]) }}" class="button tiny success round">Return Book</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    @else
+        <p>Currently, there are no books borrowed out.</p>
+    @endif
+
 @endsection
