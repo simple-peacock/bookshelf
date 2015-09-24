c14d5797dc7cd3461d123f934275553b5576a1b7
Peter <peter@simplepeacock.com>
2015-09-24 12:26:41 +0000

    Added route for returning books and minor tweak to our book resource route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,7 @@
 <?php
 
-Route::resource('book', 'BookController');
+// RESTful route for our book resource - only using 3 routes
+Route::resource('book', 'BookController',['only' => ['index', 'create', 'store']]);
+
+// route for returning books
+Route::get('book/{id}/return', 'BookController@returnBook');
